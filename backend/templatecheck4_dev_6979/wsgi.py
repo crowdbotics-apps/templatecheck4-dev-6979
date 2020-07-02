a7a7f1b1c333ca650ae32cbb097dc60d2ea01e15
Crowdbotics <team@crowdbotics.com>
2020-07-02 19:56:52 +0000

    Update models

--- backend/templatecheck4_dev_6979/wsgi.py
+++ backend/templatecheck4_dev_6979/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'templatecheck4_dev_6979.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "templatecheck4_dev_6979.settings")
 
 application = get_wsgi_application()
